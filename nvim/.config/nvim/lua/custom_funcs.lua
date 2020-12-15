9ac6e92c50642d1cef3894817bff6f2dc7bc4252
Abhijith Sethuraj <>
2020-12-15 14:45:52 +0530

    add custom function to set modeline

--- nvim/.config/nvim/lua/custom_funcs.lua
+++ nvim/.config/nvim/lua/custom_funcs.lua
@@ -12,3 +12,11 @@ function Zoom_toggle()
         vim.g.zoomed = true
     end
 end
+
+function Set_modeline()
+	cms = vim.bo.commentstring
+	line_to_insert = cms:gsub("%%s", "")
+	modeline_params = string.format(" vim: set ts=%d sw=%d sts=%d %set :", vim.bo.tabstop, vim.bo.shiftwidth, vim.bo.softtabstop, vim.bo.expandtab and "" or "no")
+	line_to_insert = line_to_insert .. modeline_params
+	vim.fn.append(vim.fn.line("$"), line_to_insert)
+end
