986cf55ddc9d3f35057de074b819aca17b95e620
Abhijith Sethuraj <>
2020-12-15 14:45:30 +0530

    don't autoformat for now (causes more problems with someone else's scripts)

--- nvim/.config/nvim/lua/custom_autofuncs.lua
+++ nvim/.config/nvim/lua/custom_autofuncs.lua
@@ -1,3 +1,4 @@
 vim.cmd("autocmd Filetype python setlocal omnifunc=v:lua.vim.lsp.omnifunc")
-vim.cmd("autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 1000)")
+-- vim.cmd("autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 1000)")
 vim.cmd("autocmd BufEnter */tmp/odin_model_* set filetype=yaml")
+vim.cmd("autocmd BufEnter *.pp set filetype=puppet")
